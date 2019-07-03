86316c4d8a39ff125435052b2d4a926da4c6a700
Chris Kirney <chris.kirney@canada.ca>
2019-07-03 14:49:10 -0400

    Now adding all files in gem directory.

--- transfer_res_s3.rb
+++ transfer_res_s3.rb
@@ -1,16 +1,19 @@
 #!/usr/bin/env ruby
 
-puts "Before: #{$LOAD_PATH}"
-curr_dir = Dir.pwd
-puts curr_dir
-gem_loc = curr_dir + '/vendor/bundle/ruby/2.2.0/gems/'
+# puts "Before: #{$LOAD_PATH}"
+# curr_dir = Dir.pwd
+# puts curr_dir
+# gem_loc = curr_dir + '/vendor/bundle/ruby/2.2.0/gems/'
+gem_loc = '/usr/local/lib/ruby/gems/2.2.0/gems/'
 puts gem_loc
 gem_dirs = Dir.entries(gem_loc).select {|entry| File.directory? File.join(gem_loc,entry) and !(entry =='.' || entry == '..') }
 puts gem_dirs
-gem_dirs.each do |gem_dir|
-  lib = gem_loc + gem_dir + '/lib'
-  $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-  puts lib
+puts 'location of gems:'
+gem_dirs.each.sort do |gem_dir|
+  lib_loc = ''
+  lib_loc = gem_loc + gem_dir + '/lib'
+  $LOAD_PATH.unshift(lib_loc) unless $LOAD_PATH.include?(lib_loc)
+  puts lib_loc
 end
 puts "After: #{$LOAD_PATH}" 
 
