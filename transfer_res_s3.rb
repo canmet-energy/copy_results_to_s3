63900bea372b29ee36b68382b19f3e208f72c9b1
Chris Kirney <chris.kirney@canada.ca>
2019-07-04 10:05:01 -0400

    Removing some debug code and adding return state of upload.

--- transfer_res_s3.rb
+++ transfer_res_s3.rb
@@ -1,21 +1,12 @@
 #!/usr/bin/env ruby
 
-# puts "Before: #{$LOAD_PATH}"
-# curr_dir = Dir.pwd
-# puts curr_dir
-# gem_loc = curr_dir + '/vendor/bundle/ruby/2.2.0/gems/'
 gem_loc = '/usr/local/lib/ruby/gems/2.2.0/gems/'
-puts gem_loc
 gem_dirs = Dir.entries(gem_loc).select {|entry| File.directory? File.join(gem_loc,entry) and !(entry =='.' || entry == '..') }
-puts gem_dirs
-puts 'location of gems:'
 gem_dirs.sort.each do |gem_dir|
   lib_loc = ''
   lib_loc = gem_loc + gem_dir + '/lib'
   $LOAD_PATH.unshift(lib_loc) unless $LOAD_PATH.include?(lib_loc)
-  puts lib_loc
-end
-puts "After: #{$LOAD_PATH}" 
+end 
 
 require 'bundler'
 require 'securerandom'
@@ -36,12 +27,8 @@ bucket = 'btapresultsbucket'
 name = File.basename(save_file)
 
 obj = s3.bucket(bucket).object(name)
-if obj.upload_file(save_file).nil?
-  puts "Upload worked!"
-  puts outfilename
-else
-  puts "Boooooooooooooooooooo!"
-end
+return_state = obj.upload_file(save_file)
+puts return_state
 
 s3.buckets.limit(50).each do |b|
   puts "#{b.name}"
