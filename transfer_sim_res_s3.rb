fb8f633a28e7d177581988dde0a753ec862c3397
Chris Kirney <chris.kirney@canada.ca>
2019-07-17 14:11:58 -0400

    Inculding Gemfile and results.zip transfer script.

---
+++ transfer_sim_res_s3.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk-s3'
+require 'json'
+
+analysis_id = ARGV[0].to_s
+
+region = 'us-east-1'
+s3 = Aws::S3::Resource.new(region: region)
+bucket_name = 'btapresultsbucket'
+bucket = s3.bucket(bucket_name)
+
+res_path = "/mnt/openstudio/assets/"
+res_file = "results." + analysis_id + ".zip"
+res_file_path = res_path + res_file
+
+
+time_obj = Time.new
+curr_time = time_obj.year.to_s + "-" + time_obj.month.to_s + "-" + time_obj.day.to_s + "_" + time_obj.hour.to_s + ":" + time_obj.min.to_s + ":" + time_obj.sec.to_s + ":" + time_obj.usec.to_s
+
+if File.file?(res_file_path)
+  file_id = analysis_id + "/" + "results.zip"
+  out_obj = bucket.object(file_id)
+  resp = []
+  while out_obj.exists? == false
+    out_obj.upload_file(res_file_path)
+  end
+else
+  file_id = "log_" + curr_time
+  log_file_loc = "./" + file_id + ".txt"
+  log_file = File.open(log_file_loc, 'w')
+  log_file.puts "#{out_file} could not be found."
+  log_file.close
+  log_obj = bucket.object("log/" + file_id)
+  log_obj.upload_file(log_file_loc)
+end
