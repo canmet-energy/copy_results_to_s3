ecea64df3b5b4741afca80839d47d3ffc74b1a2a
Chris Kirney <chris.kirney@canada.ca>
2019-07-19 22:12:40 +0000

    Inculding code to create collated error.json on s3 for an analysis.  Do the same for qaqc.json (create a simulations.json file).

--- test_download.rb
+++ test_download.rb
@@ -11,34 +11,81 @@ bucket = s3.bucket(bucket_name)
 
 error_temp_file = './error_temp.json'
 error_temp_col = './error_col.json'
-error_full = []
+error_col = []
+
+qaqc_temp_file = './qaqc_temp.json'
+qaqc_temp_col = './qaqc_col.json'
+qaqc_col = []
+
+#Go through all of the objects in the s3 bucket searching for the qaqc.json and error.json objects related the current
+#analysis.
 bucket.objects.each do |bucket_info|
   unless (/#{analysis_id}/ =~ bucket_info.key.to_s).nil?
+    #Remove the / characters with _ to avoid regex problems
     replacekey = bucket_info.key.to_s.gsub(/\//, '_')
+    #Search for objects with the current analysis id that have error and .json in them and collate them into one big
+    #collated error.json object and push it to s3.
     unless (/error_/ =~ replacekey.to_s).nil? || (/\.json/ =~ replacekey.to_s).nil?
-      puts bucket_info.key.to_s
-      for index in 0..9
+      #If you find a datapoint error file try downloading it and adding the information to the error_col array of hashes.
+      for error_index in 0..9
+        puts "error index: #{error_index}"
         bucket_info.download_file(error_temp_file)
-        index = 10 if File.exist?(error_temp_file)
+        error_index = 10 if File.exist?(error_temp_file)
       end
       if File.exist?(error_temp_file)
         error_json = JSON.parse(File.read(error_temp_file))
         error_json.each do |error_out|
-          error_full << error_out
+          error_col << error_out
         end
+        # Get rid of the datapoint error.json file that was just downloaded.
         File.delete(error_temp_file)
+      else
+        puts "Could not download #{bucket_info.key}"
+      end
+    end
+    #Search for objects with the current analysis id that have qaqc and .json in them and collate them into one big
+    #collated error.json object and push it to s3.
+    unless (/qaqc_/ =~ replacekey.to_s).nil? || (/\.json/ =~ replacekey.to_s).nil?
+      #If you find a datapoint qaqc file try downloading it and adding the information to the qaqc_col array of hashes.
+      for qaqc_index in 0..9
+        puts "qaqc index: #{qaqc_index}"
+        bucket_info.download_file(qaqc_temp_file)
+        qaqc_index = 10 if File.exist?(qaqc_temp_file)
+      end
+      if File.exist?(qaqc_temp_file)
+        qaqc_json = JSON.parse(File.read(qaqc_temp_file))
+        qaqc_json.each do |qaqc_out|
+          qaqc_col << qaqc_out
+        end
+        # Get rid of the datapoint qaqc.json file that was just downloaded.
+        File.delete(qaqc_temp_file)
       else
         puts "Could not download #{bucket_info.key}"
       end
     end
   end
 end
-File.open(error_temp_col,"w") {|each_file| each_file.write(JSON.pretty_generate(error_full))}
-out_id = analysis_id + "/" + "error_col.json"
-out_obj = bucket.object(out_id)
-while out_obj.exists? == false
-  out_obj.upload_file(error_temp_col)
+#Generated a collated error.json file using the collated array of datapoint error hashes.
+#Create an s3 object and push the collated error.json file to it.
+File.open(error_temp_col,"w") {|each_file| each_file.write(JSON.pretty_generate(error_col))}
+error_out_id = analysis_id + "/" + "error_col.json"
+error_out_obj = bucket.object(error_out_id)
+while error_out_obj.exists? == false
+  error_out_obj.upload_file(error_temp_col)
 end
+
+#Generated a collated qaqc.json file using the collated array of datapoint qaqc hashes.
+#Create an s3 object and push the collated qaqc.json file to it (this makes the simulations.json for the analysis).
+File.open(qaqc_temp_col,"w") {|each_file| each_file.write(JSON.pretty_generate(qaqc_col))}
+qaqc_out_id = analysis_id + "/" + "simulations.json"
+qaqc_out_obj = bucket.object(qaqc_out_id)
+while qaqc_out_obj.exists? == false
+  qaqc_out_obj.upload_file(qaqc_temp_col)
+end
+
+#Delete the collated error.json and qaqc.json files.
+File.delete(error_temp_col)
+File.delete(qaqc_temp_col)
 #puts analysis_id
 
 #res_path = "/mnt/openstudio/server/assets/"
