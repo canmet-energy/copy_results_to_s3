32c0932dca67900303bbedb5281722ab9f417f80
Chris Kirney <chris.kirney@canada.ca>
2019-09-30 15:30:18 +0000

    Adjusting to include analysis name in results.

--- lambda_compile_results.rb
+++ lambda_compile_results.rb
@@ -15,23 +15,11 @@ require 'aws-sdk-lambda'
 require 'json'
 require 'rest-client'
 
-def invoke_lambda(osa_id:, bucket_name:, object_keys:)
+def invoke_lambda(osa_id:, bucket_name:, object_keys:, analysis_json:)
   puts 'Object keys passed to invoke_lambda method:'
   puts object_keys
   region = 'us-east-1'
   client = Aws::Lambda::Client.new(region: region, http_read_timeout: 1800)
-  analysis_info = JSON.parse(RestClient.get("http://web:80/analyses/#{osa_id}.json", headers={}))
-  if analysis_info.nil?
-    analysis_json = {
-        analysis_id: osa_id,
-        analysis_name: 'no_name'
-    }
-  else
-    analysis_json = {
-        analysis_id: analysis_info['analysis']['_id'],
-        analysis_name: analysis_info['analysis']['display_name']
-    }
-  end
   resp_col = []
   block_size = 500.0
   cycles = (object_keys.size/block_size)
@@ -67,12 +55,13 @@ end
 
 # This calls a lambda function which collects the names of all of the objects in an s3 bucket with the analysis_id in
 # the name and that have .zip at the end.
-def get_analysis_objects(osa_id:, bucket_name:)
+def get_analysis_objects(osa_id:, bucket_name:, analysis_json:)
   region = 'us-east-1'
   client = Aws::Lambda::Client.new(region: region, http_read_timeout: 1800)
   req_payload = {
       osa_id: osa_id,
-      bucket_name: bucket_name
+      bucket_name: bucket_name,
+      analysis_name: analysis_json[:analysis_name]
   }
   payload = JSON.generate(req_payload)
   resp = client.invoke({
@@ -87,14 +76,15 @@ def get_analysis_objects(osa_id:, bucket_name:)
   return ret_objects
 end
 
-def col_res(osa_id:, bucket_name:, cycles:, file_pref:)
+def col_res(osa_id:, bucket_name:, cycles:, file_pref:, analysis_json:)
   region = 'us-east-1'
   client = Aws::Lambda::Client.new(region: region, http_read_timeout: 1800)
   req_payload = {
       osa_id: osa_id,
       bucket_name: bucket_name,
       cycle_count: cycles,
-      append_tag: file_pref
+      append_tag: file_pref,
+      analysis_id: analysis_json
   }
   puts "Ammend BTAP results payload:"
   puts req_payload
@@ -111,6 +101,22 @@ def col_res(osa_id:, bucket_name:, cycles:, file_pref:)
   return ret_objects
 end
 
+def get_analysis_info(osa_id:)
+  analysis_info = JSON.parse(RestClient.get("http://web:80/analyses/#{osa_id}.json", headers={}))
+  if analysis_info.nil?
+    analysis_json = {
+        analysis_id: osa_id,
+        analysis_name: 'no_name'
+    }
+  else
+    analysis_json = {
+        analysis_id: analysis_info['analysis']['_id'],
+        analysis_name: analysis_info['analysis']['display_name']
+    }
+  end
+  return analysis_json
+end
+
 #Get the analysis_id from the server finalization script.
 input_arguments = ARGV
 analysis_id = input_arguments[0].to_s
@@ -124,7 +130,8 @@ curr_time = time_obj.year.to_s + "-" + time_obj.month.to_s + "-" + time_obj.day.
 #lambda function.  Otherwise put an error log up saying that the analysis has not started.  This check is required
 #because OpenStudio_server 2.8.1 run the server finalization script at the start and end of the analysis rather than
 #just at the end.
-analysis_objects = get_analysis_objects(osa_id: analysis_id, bucket_name: bucket_name)
+analysis_json = get_analysis_info(osa_id: analysis_id)
+analysis_objects = get_analysis_objects(osa_id: analysis_id, bucket_name: bucket_name, analysis_json: analysis_json)
 object_keys = JSON.parse(analysis_objects["body"])
 if object_keys.empty?
   region = 'us-east-1'
@@ -139,7 +146,7 @@ else
   # error_col_#.json and simulations_#.json files in sets of 1000.  Once successfully completed another lambda
   # function is called which collates the error_col_#.json and simulations_#.json files into master error_col.json
   # and simulations.json files.
-  col_lambda_resp, cycles = invoke_lambda(osa_id: analysis_id, bucket_name: bucket_name, object_keys: object_keys)
+  col_lambda_resp, cycles = invoke_lambda(osa_id: analysis_id, bucket_name: bucket_name, object_keys: object_keys, analysis_json: analysis_json)
   #Need to fix this so it actually checks for a response.
   if col_lambda_resp.empty? || col_lambda_resp[0].nil? || cycles.nil?
     "There was an error in the lambda function which compiles the osw files into error_col and simulations files."
@@ -149,7 +156,7 @@ else
     file_prefix = ['error_col', 'simulations']
     file_prefix.each do |file_pref|
       puts file_pref
-      col_res_resp = col_res(osa_id: analysis_id, bucket_name: bucket_name, cycles: ammend_cycles, file_pref: file_pref)
+      col_res_resp = col_res(osa_id: analysis_id, bucket_name: bucket_name, cycles: ammend_cycles, file_pref: file_pref, analysis_json: analysis_json)
       col_res_resp_all << col_res_resp
     end
   end
